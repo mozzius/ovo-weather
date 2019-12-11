0e3a4087998dd6d311a3223d61ca4427a7765bfa
Samuel Newman <mozzius@protonmail.com>
2019-12-11 19:34:38 +0000

    finished the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import styles from './App.css';
+import styles from './App.module.css';
 import City from './components/City'
-import useWeather from './useWeather'
+import useWeather from './hooks/useWeather'
 
 function App() {
-    const weather = useWeather('https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22')
+    const url = 'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22'
+    // I was getting Cross-Origin Resource Sharing errors
+    // so I used this rather hacky service to get the data
+    const weather = useWeather('https://cors-anywhere.herokuapp.com/' + url)
+    // In a real app I'd pass it through a backend or perhaps use some
+    // fancy serverless functions but that's a little out of scope for this
 
+    // the <City /> is self-contained, so you could have more than one if you
+    // wanted to
     return (
         <div className={styles.appContainer}>
             <div className={styles.app}>
