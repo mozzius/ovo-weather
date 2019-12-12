4d5ef3ef6cdd1ee26805f762b9b3b733dc331100
Mozzius <mozzius.iii@gmail.com>
2019-12-12 17:31:46 +0000

    now splits data into days

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -5,7 +5,7 @@ import useWeather from '../hooks/useWeather'
 
 const City = () => {
     // in a real app you'd probably receive this url from props
-    const url = 'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22'
+    const url = 'samples.openweathermap.org/data/2.5/forecast?id=524901&appid=b6907d289e10d714a6e88b30761fae22'
     // I was getting Cross-Origin Resource Sharing errors
     // so I used this rather hacky service to get the data
     const { type, data } = useWeather('https://cors-anywhere.herokuapp.com/' + url)
@@ -22,15 +22,67 @@ const City = () => {
                 </div>
             )
         case 'success':
-            console.log(data)
+            // split the list into days
+            let days = []
+            let currentDate = null
+            data.list.forEach(day => {
+                const date = day.dt_txt.split(' ')[0]
+                if (date !== currentDate) {
+                    currentDate = date
+                    days.push({
+                        date,
+                        weather: [day.weather[0].main],
+                        maxTemp: Math.floor(day.main.temp_max - 273.15),
+                        humidity: [day.main.humidity]
+                    })
+                } else {
+                    let current = days[days.length - 1]
+                    let temp = Math.floor(day.main.temp_max - 273.15)
+
+                    current.weather.push(day.weather[0].main)
+                    current.humidity.push(day.main.humidity)
+
+                    days[days.length - 1] = {
+                        ...current,
+                        maxTemp: temp > current.maxTemp ? temp : current.maxTemp
+                    }
+                }
+            })
+
+            // make sure it's only 5 days
+            days = days.slice(0, 5)
+
+            // average the humidities etc
+            days = days.map(day => {
+                // find the most common weather
+                const weathers = [...new Set(day.weather)]
+                const count = new Array(weathers.length).fill(0)
+                day.weather.forEach(weather => {
+                    count[weathers.indexOf(weather)]++
+                })
+                let maxCount = 0
+                let weather = null
+                count.forEach((c, i) => {
+                    if (c > maxCount) {
+                        maxCount = c
+                        weather = weathers[i]
+                    }
+                })
+                // calculate the average humidity
+                let humidity = Math.floor(day.humidity.reduce((acc, curr) => acc + curr) / day.humidity.length)
+                return {
+                    ...day,
+                    weather,
+                    humidity
+                }
+            })
+
             return (
                 <div className={styles.city}>
                     <h1>{data.city.name}</h1>
                     <hr />
                     <div className={styles.days}>
-                        {data.list.slice(0, 5).map(day => {
-                            return <Day key={day.dt} data={day} />
-                        })}
+                        {days.map(day => <Day key={day.date} data={day} />)}
                     </div>
                 </div>
             )
--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import styles from './Day.module.css'
 
 const Day = ({ data }) => {
+    // convert date from ISO-8601 to normal
+    // looks like something like 2017-01-30 beforehand
+    const date = data.date.split('-').reverse().join('/')
     return (
         <div className={styles.day}>
-            <h2>{data.weather[0].main}</h2>
-            <p>Max: {Math.floor(data.main.temp_max - 273.15)}°C</p>
-            <p>Min: {Math.floor(data.main.temp_min - 273.15)}°C</p>
+            <p className={styles.date}>{date}</p>
+            <h2>{data.weather}</h2>
+            <p>Max temp: {data.maxTemp}°C</p>
+            <p>Humidity: {data.humidity}%</p>
         </div>
     )
 }
